5b6cf256c8970ed540cf06ccb77b0c7c4adb3e56
zhanyong <zhanyong@outlook.com>
2014-01-11 21:49:32 +0800

    Remove

--- mengniu/src/main/java/com/rsi/mengniu/Constants.java
+++ mengniu/src/main/java/com/rsi/mengniu/Constants.java
@@ -5,9 +5,7 @@ public final class Constants {
 	public static final String RETAILER_TESCO = "tesco";
 	public static final String RETAILER_YONGHUI = "yonghui";
 	public static final String RETAILER_METRO = "metro";
-	public static final String ROOT_PATH = "root_path";
-	// static final String TEST_ROOT_PATH = "C:/root/";
-	public static final String TEST_ROOT_PATH = "/Users/haibin/Documents/temp/";
+
 	public static final String ORDER_PATH = ".order.filePath";
 	public static final String RECEIVING_INBOUND_PATH = ".receiving.inbound.filePath";
 	public static final String RECEIVING_PROCESSED_PATH = ".receiving.processed.filePath";

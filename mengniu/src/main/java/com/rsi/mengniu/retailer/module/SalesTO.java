5e664c6d7b90b9cecc975c4f9a6da80af4f6dd6e
zhanyong <zhanyong@outlook.com>
2014-01-19 13:46:13 +0800

    Add column

--- mengniu/src/main/java/com/rsi/mengniu/retailer/module/SalesTO.java
+++ mengniu/src/main/java/com/rsi/mengniu/retailer/module/SalesTO.java
@@ -4,14 +4,16 @@ public class SalesTO extends BaseTO{
 	
 	private static final long serialVersionUID = 4087166645842465278L;
 	private String itemID = "";
+	private String itemName = "";
 	private String salesDate = "";
+	private String storeID = "";
 	private String salesQuantity = "";
 	private String salesAmount = "";
 	
 	
 
 	public String toString() {
-		return this.itemID + "\t" + this.salesDate + "\t" + this.salesQuantity
+		return this.itemID + "\t" + this.itemName + "\t" + this.salesDate + "\t" + this.storeID + "\t" + this.salesQuantity
 				+ "\t" + this.salesAmount;
 	}
 
@@ -19,33 +21,57 @@ public class SalesTO extends BaseTO{
 		String[] orderFieldArray = salesString.split("\t");
 
 		this.setItemID(orderFieldArray[0]);
-		this.setSalesDate(orderFieldArray[1]);
-		this.setSalesQuantity(orderFieldArray[2]);
-		this.setSalesAmount(orderFieldArray[3]);
+		this.setItemName(orderFieldArray[1]);
+		this.setSalesDate(orderFieldArray[2]);
+		this.setStoreID(orderFieldArray[3]);
+		this.setSalesQuantity(orderFieldArray[4]);
+		this.setSalesAmount(orderFieldArray[5]);
 	}
-	
-	
+
 	public String getItemID() {
 		return itemID;
 	}
+
 	public void setItemID(String itemID) {
 		this.itemID = itemID;
 	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
 	public String getSalesDate() {
 		return salesDate;
 	}
+
 	public void setSalesDate(String salesDate) {
 		this.salesDate = salesDate;
 	}
+
+	public String getStoreID() {
+		return storeID;
+	}
+
+	public void setStoreID(String storeID) {
+		this.storeID = storeID;
+	}
+
 	public String getSalesQuantity() {
 		return salesQuantity;
 	}
+
 	public void setSalesQuantity(String salesQuantity) {
 		this.salesQuantity = salesQuantity;
 	}
+
 	public String getSalesAmount() {
 		return salesAmount;
 	}
+
 	public void setSalesAmount(String salesAmount) {
 		this.salesAmount = salesAmount;
 	}
